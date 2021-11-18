b5128ffb02de6f2d48b9ed00c61c44f6bc4525b4
james <jamesedwarddevine@gmail.com>
2021-11-18 11:20:54 -0500

    stuck on boolean operator

---
+++ script.js
@@ -0,0 +1,6 @@
+let name="Mitch Cuckovich"
+let age="25"
+let birthday="January 24"
+
+let lifeEvents=["I was born in Troy, Michigan" , "I went to Hope College" , "I went to Junior Olympics when i was 10 years old. I placed 14th in the nation in the 800 meter event" , "I'm a graduate of a Grand Circus bootcamp"]
+
